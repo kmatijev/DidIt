9a2bd3d58b81fa3e94a8854cec9b3c01f50a3c60
@OJK0029 <karlo.mat19@gmail.com>
2024-12-30 22:14:18 +0100

    FEAT: Added notifications.

--- app/src/main/java/com/example/didit/utils/NotificationUtil.kt
+++ app/src/main/java/com/example/didit/utils/NotificationUtil.kt
@@ -3,7 +3,6 @@ package com.example.didit.utils
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.example.didit.R
 
